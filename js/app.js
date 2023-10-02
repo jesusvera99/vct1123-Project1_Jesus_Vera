c8043cb69906cab7e29ae6c012a1792a19bb5347
Jesus Vera <jesusvera@Jesuss-MacBook-Pro.local>
2023-10-02 11:57:31 -0500

    first commit to showcase subtle changes in html, css and js folders

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//javascrtipt test - A simple mouseover event to change color of the header text
+
+function mouseOver(element) {
+    element.style.color='black'
+}
+
+function mouseOut(element) {
+    element.style.color='blue'
+}
